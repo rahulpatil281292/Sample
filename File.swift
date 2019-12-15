b1005b04492c983bfa17d67178f8476dc48b3ad6
Macbook <rahulpatil281292@gmail.com>
2019-12-15 22:09:54 +0530

    Updated

---
+++ File.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Macbook on 15/12/19.
+//
+
+import Foundation
+fileprivate let personNameLabel: UILabel = {
+    let label = UILabel()
+    label.backgroundColor = .gray
+    label.text = "Please click here."
+    return label
+}()
